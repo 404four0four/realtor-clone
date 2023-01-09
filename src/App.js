11efcf729cf44e07b38a1db42ecffbbf7ca379dc
Samagra Khanna <85937988+samagra-khanna@users.noreply.github.com>
2023-01-09 22:01:45 +0530

    install firebase and react-toastify and register the user

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <>
@@ -20,6 +23,18 @@ function App() {
           <Route path="/offers" element={<Offers />} />
         </Routes>
       </Router>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+const firebaseConfig = {
+    apiKey: "AIzaSyD5hHxiHr5gzsGz1I_eLBUL-K5qjSaBdzo",
+    authDomain: "realtor-clone-822ee.firebaseapp.com",
+    projectId: "realtor-clone-822ee",
+    storageBucket: "realtor-clone-822ee.appspot.com",
+    messagingSenderId: "935166766797",
+    appId: "1:935166766797:web:6d7fa61979fc58bf3ac384"
+};
+
+initializeApp(firebaseConfig);
+export const db = getFirestore();
