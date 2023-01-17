abc7a77893385b5402c8fbf553c3a38fe01668c2
Samagra Khanna <85937988+samagra-khanna@users.noreply.github.com>
2023-01-17 20:44:12 +0530

    Add listings of user on profile page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -111,6 +111,7 @@ export default function CreateListing() {
             ...formData,
             imgUrls,
             timestamp: serverTimestamp(),
+            userRef: auth.currentUser.uid
         }
         delete formDataCopy.images;
         !formDataCopy.offer && delete formDataCopy.discountedPrice;
