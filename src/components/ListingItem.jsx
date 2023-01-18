7bcd8e35761b1a3dc92d1a8d112308a668fd3040
Samagra Khanna <85937988+samagra-khanna@users.noreply.github.com>
2023-01-18 22:41:12 +0530

    added delete listing to my listing

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import Moment from 'react-moment'
-import { Link } from 'react-router-dom'
-import { MdLocationOn } from "react-icons/md"
+import React from 'react';
+import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
+import { MdLocationOn, MdEdit } from "react-icons/md";
+import { FaTrash } from "react-icons/fa";
 
-export default function ListingItem({ listing, id }) {
+export default function ListingItem({ listing, id, onEdit, onDelete }) {
     return (
         <li className='relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition ease-in-out duration-150 m-[10px]'>
             <Link className='contents' to={`/category/${listing.type}/${id}`}>
@@ -36,6 +37,12 @@ export default function ListingItem({ listing, id }) {
                     </div>
                 </div>
             </Link >
+            {onDelete && (
+                <FaTrash className='absolute bottom-2 right-2 h-[14px] cursor-pointer text-red-500' onClick={() => onDelete(listing.id)} />
+            )}
+            {onEdit && (
+                <MdEdit className='absolute bottom-2 right-7 h-4 cursor-pointer text-black' onClick={() => onEdit(listing.id)} />
+            )}
         </li>
     );
 };
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getAuth, updateProfile, } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { collection, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
 import { db } from "../firebase";
 import { FcHome } from 'react-icons/fc';
 import ListingItem from "../components/ListingItem";
@@ -60,6 +60,19 @@ export default function Profile() {
     }
     fetchUserListings();
   }, [auth.currentUser.uid]);
+  async function onDelete(listingID) {
+    if (window.confirm("Are you sure about deleting your listing?")) {
+      await deleteDoc(doc(db, "listings", listingID));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      );
+      setListings(updatedListings);
+      toast.success("Your listing has been deleted.")
+    }
+  }
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`);
+  }
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -92,7 +105,7 @@ export default function Profile() {
             <h2 className="text-2xl text-center font-semibold mb-6 mt-6">My Listings</h2>
             <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6">
               {listings.map((listing) => (
-                <ListingItem key={listing.id} id={listing.id} listing={listing.data} />
+                <ListingItem key={listing.id} id={listing.id} listing={listing.data} onDelete={() => onDelete(listing.id)} onEdit={() => onEdit(listing.id)} />
               ))}
             </ul>
           </>
