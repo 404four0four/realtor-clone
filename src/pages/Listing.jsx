b79f0a8e5417e332b86871c13db1dc51d4253be0
Samagra Khanna <85937988+samagra-khanna@users.noreply.github.com>
2023-01-19 19:17:02 +0530

    Add share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,11 +6,13 @@ import Spinner from "../components/Spinner";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from "swiper";
 import 'swiper/css/bundle';
+import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
     const params = useParams();
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [shareLinkCopied, setShareLinkCopied] = useState(false);
     SwiperCore.use([Autoplay, Navigation, Pagination])
     useEffect(() => {
         async function fetchListing() {
@@ -37,6 +39,18 @@ export default function Listing() {
                     </SwiperSlide>
                 ))}
             </Swiper>
+            <div onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setShareLinkCopied(true);
+                setTimeout(() => {
+                    setShareLinkCopied(false);
+                }, 2000)
+            }} className='fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center'>
+                <FaShare className='text-lg text-slate-500' />
+            </div>
+            {shareLinkCopied && (
+                <p className='fixed top-[23%] right-[5%] z-10 font-semibold border-2 border-gray-400 bg-white rounded-md p-2'>Link copied</p>
+            )}
         </main>
     )
 }
