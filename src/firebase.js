cfda3c1d276a2f7a680cb156c8d28eba93809c34
Samagra Khanna <85937988+samagra-khanna@users.noreply.github.com>
2023-01-19 17:26:49 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyD5hHxiHr5gzsGz1I_eLBUL-K5qjSaBdzo',
-    authDomain: "realtor-clone-822ee.firebaseapp.com",
-    projectId: "realtor-clone-822ee",
-    storageBucket: "realtor-clone-822ee.appspot.com",
-    messagingSenderId: "935166766797",
-    appId: "1:935166766797:web:6d7fa61979fc58bf3ac384"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 initializeApp(firebaseConfig);
